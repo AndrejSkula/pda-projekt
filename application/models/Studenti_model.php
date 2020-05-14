5f3e7cc847bceeb6eff3b66cbadb10a635d4611d
AndrejSkula <64094209+AndrejSkula@users.noreply.github.com>
2020-05-14 11:28:29 +0200

    Uprava udajov zo seminara

---
+++ application/models/Studenti_model.php
@@ -0,0 +1,45 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Studenti_model extends CI_Model {
+	public function __construct()
+	{
+		$this->load->database();
+	}
+
+	function ZobrazStudentov($id="") {
+		if(!empty($id)){
+			$query = $this->db->get_where('studenti', array('id_studenta' => $id));
+			return $query->row_array();
+		}else{
+			$query = $this->db->get('studenti');
+			return $query->result_array();
+		}
+
+	}
+	// vlozenie zaznamu
+	public function insert($data = array()) {
+		$insert = $this->db->insert('studenti', $data);
+		if($insert){
+			return $this->db->insert_id();
+		}else{
+			return false;
+		}
+	}
+
+	// aktualizacia zaznamu
+	public function update($data, $id) {
+		if(!empty($data) && !empty($id)){
+			$update = $this->db->update('studenti', $data, array('id_studenta'=>$id));
+			return $update?true:false;
+		}else{
+			return false;
+		}
+	}
+
+	// odstranenie zaznamu
+	public function delete($id){
+		$delete = $this->db->delete('studenti',array('id_studenta'=>$id));
+		return $delete?true:false;
+	}
+
+}
---
+++ application/views/studenti/index.php
@@ -0,0 +1,47 @@
+<div class="container">
+	<?php if(!empty($success_msg)){ ?>
+		<div class="col-xs-12">
+			<div class="alert alert-success"><?php echo $success_msg; ?></div>
+		</div>
+	<?php }elseif(!empty($error_msg)){ ?>
+		<div class="col-xs-12">
+			<div class="alert alert-danger"><?php echo $error_msg; ?></div>
+		</div>
+	<?php } ?>
+	<div class="row">
+		<h1>Zoznam študentov</h1>
+	</div>
+	<div class="row">
+		<div class="col-xs-12">
+			<div class="panel panel-default ">
+				<div class="panel-heading">Študenti <a href="<?php echo site_url('studenti/add/'); ?>" class="glyphicon glyphicon-plus pull-right" ></a></div>
+				<table class="table striped">
+					<thead>
+					<tr>
+						<th width="20%">ID</th>
+						<th width="30%">Meno</th>
+						<th width="30%">Priezvisko</th>
+						<th width="20%">Akcia</th>
+					</tr>
+					</thead>
+					<tbody id="userData">
+					<?php if(!empty($studenti)): foreach($studenti as $student): ?>
+						<tr>
+							<td><?php echo '#'.$student['id_studenta']; ?></td>
+							<td><?php echo $student['meno']; ?></td>
+							<td><?php echo $student['priezvisko']; ?></td>
+							<td>
+								<a href="<?php echo site_url('studenti/view/'.$student['id_studenta']); ?>"class="glyphicon glyphicon-eye-open"></a>
+								<a href="<?php echo site_url('studenti/edit/'.$student['id_studenta']); ?>"class="glyphicon glyphicon-edit"></a>
+								<a href="<?php echo site_url('studenti/delete/'.$student['id_studenta']); ?>"class="glyphicon glyphicon-trash" onclick="return confirm('Are you sure to delete?')"></a>
+							</td>
+						</tr>
+					<?php endforeach; else: ?>
+						<tr><td colspan="4">Žiadni študenti ......</td></tr>
+					<?php endif; ?>
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</div>
+</div>
